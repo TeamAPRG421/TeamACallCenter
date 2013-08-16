9b960b961a53e215a72dd4b03d8fd89f95be5d7c
Shane <shane.maher@me.com>
2013-08-15 18:40:39 -0700

    blah blah

---
+++ src/logic/Caller.java
@@ -0,0 +1,21 @@
+package logic;
+	
+public class Caller {
+	
+	public int ID;
+	public String CustomerName;
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getCustomerName() {
+		return CustomerName;
+	}
+	public void setCustomerName(String customerName) {
+		CustomerName = customerName;
+	}
+	
+	
+}
