20cbd4ccbdccf4bd04087242f16cfb38e3b6cf9f
Sadie <23sadie@gmail.com>
2013-08-15 18:48:06 -0700

    update caller class
    I updated the Caller.java file to include attributes about the call
    itself. I created the getters and setters for these attributes.

--- src/logic/Caller.java
+++ src/logic/Caller.java
@@ -1,9 +1,35 @@
 package logic;
+
+import java.util.Date;
 	
 public class Caller {
 	
-	public int ID;
-	public String CustomerName;
+	private int ID;
+	private String CustomerName;
+	private String note;
+	private Date startTime;
+	private Date endTime;
+	
+	
+	
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public Date getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	public Date getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
 	public int getID() {
 		return ID;
 	}
